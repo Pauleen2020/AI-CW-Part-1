d667b28e73f34802896148983d4ec598503c784c
gherkins05 <finlaysperkins@gmail.com>
2025-11-18 12:08:53 +0000

    Modified homogenous array spec

--- generate_population.m
+++ generate_population.m
@@ -1,7 +1,7 @@
 function population = generate_population(POPULATION_SIZE, INPUT_SIZE, OUTPUT_SIZE)
-    population = zeros(1, POPULATION_SIZE);
+    population = [];
 
     for i = 1:POPULATION_SIZE
-        population(i) = generate_spider(INPUT_SIZE, OUTPUT_SIZE);
+        population(end+1) = generate_spider(INPUT_SIZE, OUTPUT_SIZE);
     end
 end
