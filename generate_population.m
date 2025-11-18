78d9aa3169a7c4aa67afbe2ffc16c34c155fc92c
gherkins05 <finlaysperkins@gmail.com>
2025-11-18 12:13:09 +0000

    t

--- generate_population.m
+++ generate_population.m
@@ -1,5 +1,5 @@
 function population = generate_population(POPULATION_SIZE, INPUT_SIZE, OUTPUT_SIZE)
-    population = [];
+    population = {};
 
     for i = 1:POPULATION_SIZE
         population(end+1) = generate_spider(INPUT_SIZE, OUTPUT_SIZE);
